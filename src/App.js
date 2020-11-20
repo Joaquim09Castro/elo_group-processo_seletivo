547f6052b2c2b19ec64e589c1a9913ffec86c9dc
Joaquim Castro <joaquimcastro2909@gmail.com>
2020-11-20 18:20:02 -0200

    Pannel Component created

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
 
 import Register from './Components/Register/Register';
+import Pannel from './Components/Pannel/Pannel';
 import Page404 from './Components/Page404/Page404';
 
 
@@ -18,7 +19,7 @@ function App() {
 
         <Switch>
           <Route exact path="/" children={<Register />} />
-          {/* <Route path="/" children={}/> */}
+          <Route path="/pannel" children={<Pannel />}/>
           <Route path="*" children={<Page404 />}/>
         </Switch>
       
